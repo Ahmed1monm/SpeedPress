87682b2305321f0c59107228f5af359a93dba6a5
MONE'M <ahmedmoniem922@gmail.com>
2024-09-28 17:33:27 +0300

    update readme and add tests

--- README.md
+++ README.md
@@ -1,17 +1,47 @@
-## EzPress !!!
+# EzPress
 
 A CLI tool to initialize a new express project with in TypeScript with a clean architecture and a set of files that are commonly used in every project.
 
 All files have the necessary, commonly used code to get started with a new project.
-### Project Structure
-![Project Structure](./images/tree.jpeg)
 
-### Installation
+## Project Structure
+
+```Bash
+.
+├── dist
+├── Dockerfile
+├── package.json
+├── package-lock.json
+├── README.md
+├── src
+│   ├── config
+│   │   └── index.ts
+│   ├── controllers
+│   │   └── index.ts
+│   ├── index.ts
+│   ├── middlewares
+│   │   └── index.ts
+│   ├── models
+│   │   └── index.ts
+│   ├── routes
+│   │   └── index.ts
+│   ├── services
+│   │   └── index.ts
+│   └── utils
+│       └── index.ts
+├── swagger.yaml
+├── tests
+└── tsconfig.json
+```
+
+## Installation
+
 ```bash
   npm install -g ez-press
 ```
 
 ### Usage
+
 ```bash
   ez-press init <project-name>
 ```
--- tests/commands.test.ts
+++ tests/commands.test.ts
@@ -1,5 +1,7 @@
-import {describe, jest, beforeEach, it} from '@jest/globals';
+import {describe, jest, beforeEach, it, expect} from '@jest/globals';
 import {initHandler} from '../src/commands/init';
+import fs from 'fs';
+import { afterEach } from 'node:test';
 
 jest.mock('child_process');
 
@@ -10,10 +12,26 @@ describe('initHandler', () => {
 
 	it('should initialize project with default name if no name is provided', async () => {
 		await initHandler();
+		
+		const defaultProjectName = './ezpress-created-app';
+		const dirs = ['src', 'tests', 'dist'];
+		const readedProjectDir = fs.readdirSync(defaultProjectName);
+		const currentReadedDir = fs.readdirSync('.');
+
+		expect(currentReadedDir).toContain('ezpress-created-app');
+		
+		dirs.forEach(dir => {
+			expect(readedProjectDir).toContain(dir);
+		});
 	});
 
 	it('should initialize project with the provided name', async () => {
 		const projectName = 'my-awesome-app';
 		await initHandler(projectName);
+	});
+
+	afterEach(() => {
+		fs.rmdirSync('./ezpress-created-app', {recursive: true});
+		fs.rmdirSync('./my-awesome-app', {recursive: true});
 	});
 });
